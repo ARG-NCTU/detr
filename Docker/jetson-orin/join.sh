18d7b3fef5a2cbd1a25325eacacfa3e3e81847b3
zhuchi76 <tzuchichen.en12@nycu.edu.tw>
2024-12-26 17:55:03 +0800

    fix(docker): remove DISPLAY environment variable
    The DISPLAY environment variable was removed from the Docker exec
    command in join.sh for Jetson Orin. This change ensures that the
    script runs without requiring the DISPLAY variable, which may not
    be necessary for all use cases.

--- Docker/jetson-orin/join.sh
+++ Docker/jetson-orin/join.sh
@@ -11,7 +11,6 @@ xhost +
 containerid=$(docker ps -aqf "ancestor=${IMG}") && echo $containerid
 docker exec -it \
     --privileged \
-    -e DISPLAY=${DISPLAY} \
     -e LINES="$(tput lines)" \
     ${containerid} \
     $BASH_OPTION
